0f5a9afdc55fa1f23ef1730e9bdaa3c40c3f28ea
Savera <Level 2>
2016-12-28 11:50:46 -0800

    hi Dust how your day

--- src/day1/robot/MyFirstJavaProgram.java
+++ src/day1/robot/MyFirstJavaProgram.java
@@ -7,9 +7,21 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
+Robot Dust=new Robot();
+Dust.setSpeed(200);
+Dust.setPenColor(0,0,255);
+Dust.penDown();
+for (int i = 0; i < 4; i++) {
+	Dust.move(300);
+Dust.turn(90);
+Dust.move(300);		
+Dust.turn(90);
+Dust.move(300);
+Dust.turn(90);
+Dust.move(300);
+}
+;               
 
-	
-		
-		
-	}
+
+}
 }
